5654c5a8fdf18bd1ce53ccefbc75175c7be8d233
Miguel Hernandez <phyzix5761@gmail.com>
2016-10-08 22:20:49 -0400

    Added comments

--- DocSimilarity.py
+++ DocSimilarity.py
@@ -1,20 +1,28 @@
 import collections
 
 
+# Returns a float between 0 and 1 representing how
+# similar two documents are to each other
 def get_similarity(doc1, doc2):
-    document_distance = 0
+    word_match_occurrence = 0
 
     doc1_word_map, doc1_word_count = get_word_map(doc1)
     doc2_word_map, doc2_word_count = get_word_map(doc2)
 
+    # Iterate through each key value pair of doc1 word map
+    # and check if the key is doc2 word map
     for key, value in doc1_word_map.items():
 
+        # If the value is present then add twice the lowest value to the word match occurrence counter.
+        # (This is the total number of times that word appears in BOTH bodies of text)
         if key in doc2_word_map:
-            document_distance += min(value, doc2_word_map[key]) * 2
+            word_match_occurrence += min(value, doc2_word_map[key]) * 2
 
-    return document_distance / (doc1_word_count + doc2_word_count)
+    # Return the ratio between the occurrence of the word matches and the count of all words
+    return word_match_occurrence / (doc1_word_count + doc2_word_count)
 
 
+# Returns a map and count of words appearing in a given document
 def get_word_map(doc):
     word_map = collections.defaultdict(int)
     word_count = 0
